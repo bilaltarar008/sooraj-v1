ec0cff82f8ba1a4b4d351cb8cfc801d06f5c19c1
Bilal Arshad Tarar <bilal.tarar008@gmail.com>
2026-01-13 16:11:25 +0500

    ADD NPM FIREBSE

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDz5MU9psndQYhwxaTKe0eJMCrmutdKOUA",
+  apiKey: "AIzaSyDz5MU9psndQYhwxaTKe0eJMCmutdKOUA",
   authDomain: "sooraj-crop-sciences-21b09.firebaseapp.com",
   projectId: "sooraj-crop-sciences-21b09",
   storageBucket: "sooraj-crop-sciences-21b09.appspot.com",
   messagingSenderId: "801342188260",
   appId: "1:801342188260:web:18b6ca91846e609c265af1",
-  measurementId: "G-YMMRFBXLXP5",
+  measurementId: "G-YMMRFBLXP5"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// Safe Analytics (works in incognito too)
-isSupported().then((supported) => {
-  if (supported) {
-    getAnalytics(app);
-  }
-});
+// Analytics only works in browser
+if (typeof window !== "undefined") {
+  getAnalytics(app);
+}
+
+export default app;
