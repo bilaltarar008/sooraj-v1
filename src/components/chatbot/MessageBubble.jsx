bb104aa11fe84056362ee907621c820afe98f909
bilal after download zip file <bilal.tarar008@gmail.com>
2026-02-27 01:13:48 +0500

    Resolved merge conflict

--- src/components/chatbot/MessageBubble.jsx
+++ src/components/chatbot/MessageBubble.jsx
@@ -1,36 +1,36 @@
-// import React from "react";
-// import "./chatbot.css";
+  // import React from "react";
+  // import "./chatbot.css";
 
-// const MessageBubble = ({ sender, text }) => {
-//     const urduRegex = /[\u0600-\u06FF]/;
-//     const dir = urduRegex.test(text) ? "rtl" : "ltr";
-//     return (
-//       <div className={`message ${sender}`} style={{ direction: dir }}>
-//         <p>{text}</p>
-//       </div>
-//     );
-//   };
+  // const MessageBubble = ({ sender, text }) => {
+  //     const urduRegex = /[\u0600-\u06FF]/;
+  //     const dir = urduRegex.test(text) ? "rtl" : "ltr";
+  //     return (
+  //       <div className={`message ${sender}`} style={{ direction: dir }}>
+  //         <p>{text}</p>
+  //       </div>
+  //     );
+  //   };
 
-// export default MessageBubble;
+  // export default MessageBubble;
 
-import React from "react";
-import "./chatbot.css";
+  import React from "react";
+  import "./chatbot.css";
 
-const MessageBubble = ({ sender, text, audio }) => {
-  const urduRegex = /[\u0600-\u06FF]/;
-  const dir = text && urduRegex.test(text) ? "rtl" : "ltr";
+  const MessageBubble = ({ sender, text, audio }) => {
+    const urduRegex = /[\u0600-\u06FF]/;
+    const dir = text && urduRegex.test(text) ? "rtl" : "ltr";
 
-  return (
-    <div className={`message ${sender}`} style={{ direction: dir }}>
-      {audio ? (
-        <audio controls className="audio-player">
-          <source src={audio} type="audio/webm" />
-        </audio>
-      ) : (
-        <p>{text}</p>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className={`message ${sender}`} style={{ direction: dir }}>
+        {audio ? (
+          <audio controls className="audio-player">
+            <source src={audio} type="audio/webm" />
+          </audio>
+        ) : (
+          <p>{text}</p>
+        )}
+      </div>
+    );
+  };
 
-export default MessageBubble;
+  export default MessageBubble;
